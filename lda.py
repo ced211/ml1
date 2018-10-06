3ef436cee03c2316fe408241a81a5b0f1dbe51cc
Pseudoless <julienlhoest@hotmail.com>
2018-10-06 22:49:03 +0200

    Definition des methodes (main a faire)

--- lda.py
+++ lda.py
@@ -7,6 +7,7 @@ Project 1 - Classification algorithms
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import math
 
 from sklearn.base import BaseEstimator, ClassifierMixin
 
@@ -45,6 +46,29 @@ class LinearDiscriminantAnalysis(BaseEstimator, ClassifierMixin):
         # TODO your code here.
         # ====================
 
+        prop = [0, 0] # Binary output supposed
+        mean = np.zeros(len(X[0]))
+
+        for sample in y:
+        	if sample == 0.0:
+        		prop[0]+=1
+        	else:
+        		prop[1]+=1
+
+        for sample in X:
+        	i = 0
+        	for feature in sample:
+        		mean[i] += feature
+        		i+=1
+
+        mean /= len(y)
+
+        cov = np.cov(X, None, False)
+
+        # Statement formula
+        self.f = lambda x, k : (2* math.pi * math.sqrt(np.linalg.det(cov)))**-1 * np.exp(-0.5 * (x - mean[k]) * np.linalg.inv(cov) * np.matrix.transpose(x - mean[k]) ) 
+        self.mean = mean
+
         return self
 
     def predict(self, X):
@@ -64,8 +88,22 @@ class LinearDiscriminantAnalysis(BaseEstimator, ClassifierMixin):
         # ====================
         # TODO your code here.
         # ====================
+        n_features = len(X[0])
+        y = []
+	    densities = np.zeros(n_features)
 
-        pass
+	    for sample in X:    
+	        
+	        for k in range(n_features):
+	        	densities[k] = self.f(sample[k], k)
+
+	        den = np.dot(self.mean, densities)
+	        if (densities[0]*mean[0]/den) > (densities[1]*mean[1]/den) : # Search of the Maximum
+	        	y.append(0.0)
+	        else:
+	        	y.append(1.0)
+
+	    return y
 
     def predict_proba(self, X):
         """Return probability estimates for the test data X.
@@ -85,8 +123,22 @@ class LinearDiscriminantAnalysis(BaseEstimator, ClassifierMixin):
         # ====================
         # TODO your code here.
         # ====================
+        n_features = len(X[0])
+        p = []
+        num = []
+		densities = np.zeros(n_features)
 
-        pass
+	    for sample in X:    
+	        
+	        for k in range(n_features):
+	        	densities[k] = self.f(sample[k], k)
+	        	num[k] = densities[k]*mean[k]
+
+	        den = np.dot(self.mean, densities)
+	        p.append( num/den )
+
+
+        return p
 
 if __name__ == "__main__":
     from data import make_data
