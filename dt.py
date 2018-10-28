dd5c7cd5fbac4fb2483d6df1b2bdbc6cb3e7a84d
ced211 <43910066+ced211@users.noreply.github.com>
2018-10-28 21:25:21 +0100

    Update dt.py

--- dt.py
+++ dt.py
@@ -10,14 +10,55 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 from data import make_dataset1, make_dataset2
+from plot import plot_boundary
 from sklearn.tree import DecisionTreeClassifier
-
-
-# (Question 1)
-
-# Put your funtions here
-# ...
-
+from sklearn.metrics import accuracy_score
 
 if __name__ == "__main__":
-    pass # Make your experiments here
+
+    train_set = make_dataset2(1200, 565354)
+    test_set = make_dataset2(300, 156)
+    seeds = [5, 36, 47, 9898]
+    depth_test = [1, 2, 4, 8]
+    scores = {}
+
+    # create tree and figure of unconstrained depth
+
+    estimator = DecisionTreeClassifier().fit(train_set[0], train_set[1])
+    plot_boundary("inf_train_tree", estimator, train_set[0], train_set[1])
+    plot_boundary("inf_test_tree", estimator, test_set[0], test_set[1])
+    prediction = estimator.predict(test_set[0])
+    scores[0] = []
+    scores[0].append(accuracy_score(test_set[1], prediction))
+
+    # part 2, test model against 5 test set.
+    for seed in seeds:
+        test_set = make_dataset2(300, seed)
+        prediction = estimator.predict(test_set[0])
+        scores[0].append(accuracy_score(test_set[1], prediction))
+
+    # create tree and figure for each depth
+    for depth in depth_test:
+        print("create tree" + str(depth))
+        estimator = DecisionTreeClassifier(
+            max_depth=depth). fit(train_set[0], train_set[1])
+        plot_boundary("test_tree_" + str(depth),
+                      estimator, test_set[0], test_set[1])
+        plot_boundary("train_tree_" + str(depth),
+                      estimator, train_set[0], train_set[1])
+        prediction = estimator.predict(test_set[0])
+        scores[depth] = []
+        scores[depth].append(accuracy_score(test_set[1], prediction))
+
+        # part 2 test over 5 generation
+
+        for seed in seeds:
+            test_set = make_dataset2(300, seed)
+            prediction = estimator.predict(test_set[0])
+            scores[depth].append(accuracy_score(test_set[1], prediction))
+
+        # print result
+
+    for depth in scores:
+        print(" for depth: " + str(depth) + " mean: " +
+              str(np.mean(scores[depth])) + " std: " + str(np.std(scores[depth])))
